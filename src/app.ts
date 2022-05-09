91b8de22ba15f822f59354a32122657df6dcd1cf
Nick Nance <nance.nick@gmail.com>
2022-05-08 20:49:17 -0500

    initial typescript setup

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,10 @@
-"use strict";
-const renderToDo = (todo) => {
+type ToDo = {
+    id: number,
+    completed: boolean,
+    note: string,
+}
+
+const renderToDo = (todo: ToDo) => {
     const { completed, note, id } = todo;
     return `
         <li ${completed ? `class="completed"` : ``}>
@@ -10,33 +15,37 @@ const renderToDo = (todo) => {
             </div>
             <input class="edit" value="${id}">
         </li>
-    `;
-};
-const renderToDos = (toDos) => {
+    `
+}
+
+const renderToDos = (toDos: ToDo[]) => {
     return `
         <ul class="todo-list">
             ${toDos.map(renderToDo).reduce((prev, cur) => prev + cur)}
         </ul>
-    `;
-};
+    `
+}
+
 const renderApp = () => {
-    // Your starting point. Enjoy the ride!
-    const main = window.document.querySelector('.main');
-    const toDos = [{
-            id: 1,
-            completed: true,
-            note: "Taste JavaScript"
-        }, {
-            id: 2,
-            completed: false,
-            note: "Buy a unicorn"
-        }];
+	// Your starting point. Enjoy the ride!
+    const main = window.document.querySelector('.main')
+    const toDos: ToDo[] = [{
+        id: 1,
+        completed: true,
+        note: "Taste JavaScript"
+    }, {
+        id: 2,
+        completed: false,
+        note: "Buy a unicorn"
+    }]
+
     if (main) {
         main.innerHTML = `
             <input id="toggle-all" class="toggle-all" type="checkbox">
             <label for="toggle-all">Mark all as complete</label>
             ${renderToDos(toDos)}
-        `;
+        `
     }
-};
+}
+
 renderApp();
