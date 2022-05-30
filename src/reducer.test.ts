179685a9dc8a9c423fa78dcb69b74c9574b6a0fc
Nick Nance <nance.nick@gmail.com>
2022-05-29 17:05:35 -0700

    full reducer test coverage

--- src/reducer.test.ts
+++ src/reducer.test.ts
@@ -1,20 +1,59 @@
 import { reducer } from "./reducer";
 import { ActionTypes, AppState, FilterType } from "./types";
 
-test("ToggleAll", () => {
+describe("reducer", () => {
     const state: AppState = {
         toDos: [{ id: 1, note: "task 1", completed: true }, { id: 2, note: "task 2", completed: false }],
         filter: FilterType.All
     };
 
-    const action = {
-        type: ActionTypes.ToggleAll
-    };
+    test("ToggleAll", () => {
+        const newState = reducer(state, {
+            type: ActionTypes.ToggleAll
+        });
 
-    const newState = reducer(state, action);
+        expect(newState).toEqual({
+            ...state,
+            toDos: [{ id: 1, note: "task 1", completed: true }, { id: 2, note: "task 2", completed: true }]
+        });
+    });
 
-    expect(newState).toEqual({
-        toDos: [{ id: 1, note: "task 1", completed: true }, { id: 2, note: "task 2", completed: true }],
-        filter: FilterType.All
+    test("SetFilter", () => {
+        const newState = reducer(state, {
+            type: ActionTypes.SetFilter,
+            payload: FilterType.Active
+        });
+
+        expect(newState).toEqual({
+            ...state,
+            filter: FilterType.Active
+        });
+    });
+
+    test("AddTodo", () => {
+        const newState = reducer(state, {
+            type: ActionTypes.AddItem,
+            payload: "task 3"
+        });
+
+        expect(newState.toDos.length).toEqual(3);
+    });
+
+    test("DestroyTodo", () => {
+        const newState = reducer(state, {
+            type: ActionTypes.DestroyItem,
+            payload: 1
+        });
+
+        expect(newState.toDos.length).toEqual(1);
+    });
+
+    test("ToggleTodo", () => {
+        const newState = reducer(state, {
+            type: ActionTypes.ToggleCompleted,
+            payload: 1
+        });
+
+        expect(newState.toDos[0].completed).toBe(false);
     });
 });
