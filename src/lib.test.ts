cbed2f549e89e5b62fda045ddb53d072ef9c587d
Nick Nance <nance.nick@gmail.com>
2022-06-04 17:59:43 -0700

    convert patches to objects instead of functions for improved testing

--- src/lib.test.ts
+++ src/lib.test.ts
@@ -1,4 +1,4 @@
-import { diff, applyPatches } from "./lib";
+import { diff, applyPatches, PatchType } from "./lib";
 
 describe.only("diff", () => {
     it("should return an empty array when the same node is passed", () => {
@@ -11,13 +11,35 @@ describe.only("diff", () => {
         const newTree = document.createElement("section");
         expect(diff(oldTree, newTree)).toHaveLength(1);
     });
+    it("should return an array with a remove patch when the node is null", () => {
+        const oldTree = document.createElement("div");
+        const newTree = null;
+        expect(diff(oldTree, newTree)).toHaveLength(1);
+    });
+    it("should return an array with a remove patch child count is different", () => {
+        const oldTree = document.createElement("div");
+        oldTree.innerHTML = `
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>`
+
+        const newTree = document.createElement("div");
+        newTree.innerHTML = `
+        <ul>
+            <li>1</li>
+            <li>2</li>
+        </ul>`
+        expect(diff(oldTree, newTree)).toHaveLength(1);
+    });
 });
 
-describe.only("applyPatches", () => {
+describe("applyPatches", () => {
     it("should append new node when missing child", () => {
         const newTree = document.createElement("section");
         const root = document.createElement("div");
-        applyPatches(root, [(parentNode) => parentNode.appendChild(newTree)]);
+        applyPatches(root, [{ type: PatchType.Append, node: newTree }]);
         expect(root.firstElementChild).toBe(newTree);
     });
     it("should replace the old node with the new node", () => {
@@ -25,7 +47,7 @@ describe.only("applyPatches", () => {
         const newTree = document.createElement("section");
         const root = document.createElement("div");
         root.appendChild(oldTree);
-        applyPatches(root, [(parentNode) => parentNode.replaceChild(newTree, oldTree)]);
+        applyPatches(root, [{ type: PatchType.Replace, node: newTree }]);
         expect(root.firstElementChild).toBe(newTree);
     });
 });
--- src/lib.ts
+++ src/lib.ts
@@ -1,14 +1,31 @@
-export const diff = (oldTree: Element | null, newTree: Element) => {
+export const diff = (oldTree: Element | null, newTree: Element | null) => {
     const patches: Patch[] = [];
-    const walk = (oldNode: Element | null, newNode: Element) => {
+    const walk = (oldNode: Element | null, newNode: Element | null) => {
 
         if (!oldNode && newNode) {
-            patches.push((parent) => parent.appendChild(newNode));
+            patches.push({
+                type: PatchType.Append,
+                node: newNode
+            })
             return;
-        } else if (oldNode) {            
+        } else if (oldNode && !newNode) {
+            patches.push({
+                type: PatchType.Remove,
+                node: oldNode
+            });
+            return;
+        } else if (oldNode && newNode) {            
             if (oldNode.nodeName !== newNode.nodeName) {
-                patches.push ((parent) => parent.replaceChild(newNode, oldNode));
+                patches.push ({
+                    type: PatchType.Replace,
+                    node: newNode,
+                });
                 return;
+            }
+            const oldChildren = oldNode.childNodes;
+            const newChildren = newNode.childNodes;
+            for (let i = 0; i < oldChildren.length; i++) {
+                walk(oldChildren[i] as Element, newChildren[i] as Element);
             }
         }
 
@@ -17,10 +34,29 @@ export const diff = (oldTree: Element | null, newTree: Element) => {
     return patches;
 }
 
-type Patch = (parentNode: ParentNode) => Element;
+export enum PatchType {
+    Append,
+    Remove,
+    Replace
+}
+
+export type Patch = {
+    type: PatchType,
+    node: Element,
+}
 
 export const applyPatches = (root: HTMLElement, patches: Patch[]) => {
+    const applyPatch = (parent: HTMLElement, patch: Patch) => {
+        if (patch.type === PatchType.Append) {
+            parent.appendChild(patch.node);
+        } else if (patch.type === PatchType.Remove) {
+            parent.removeChild(patch.node);
+        } else if (patch.type === PatchType.Replace) {
+            parent.replaceChild(patch.node, patch.node);
+        }
+    };
+
     patches.forEach(patch => {
-        patch(root);
+        applyPatch(root, patch);
     });
 }
